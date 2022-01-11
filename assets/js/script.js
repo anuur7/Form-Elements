afabd8a8cad177a62e9941557286a6229492c75a
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-01-11 20:58:28 +0000

    copied from instructor folder

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,26 @@
-var shoppingFormEl = $('#shopping-form');
-var shoppingListEl = $('#shopping-list');
+const shoppingFormElement = $('#shopping-form');
+const shoppingListElement = $('#shopping-list');
+const shoppingInputElement = $('#shopping-input');
+
+function formSubmitHandler(event) {
+    event.preventDefault();
+    const newItem = shoppingInputElement.val();
+    addNewShoppingListElement(newItem);
+}
+
+function addNewShoppingListElement(item) {
+    const newLineItem = $('<li>');
+    newLineItem.text(item);
+    shoppingListElement.append(newLineItem);
+}
+
+$('#submit-button').click(formSubmitHandler)
+
+/*
+* Wire up a function to the button
+* Pick out the input value on the form
+* Create a new line item in the shoppingListElement
+*/
 
 // TODO: Create a function to handle the form submission event that captures the form's `<input>` value and prints it to the `shoppingListEl` as a `<li>`
 
